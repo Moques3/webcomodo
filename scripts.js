f8bcc1fe9f772326972195dc621138f569412532
Monique Souza <moques3@outlook.com>
2022-05-12 11:00:51 -0300

    iniciando formulário

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// let nomeUsr;
+// let button;
+
+// button = document.getElementById("btnEnviar");
+
+// button.addEventListener('click',(e) => {
+// e.preventDefault();
+
+// nomeUsr =document.getElementById("nomeUsr").value;
+
+//     alert(nomeUsr);
+// });
+
+// $("#btnEnviar").on('click', (e) => {
+//     e.preventDefault();
+
+//     alert($("#nomeUsr").Val());
+// })
