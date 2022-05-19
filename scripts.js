30671466bf23c39adb9639778c473af901b16670
Ana <acpedroso93@gmail.com>
2022-05-19 10:58:47 -0300

    início da lógica do form

--- scripts.js
+++ scripts.js
@@ -16,3 +16,43 @@
 
 //     alert($("#nomeUsr").Val());
 // })
+
+// let parede = {
+// 'altura':0,
+// 'largura':0,
+// 'nmrPortas':0,
+// 'nmrJanelas':0
+// }
+
+let comodo =[];
+let paredeAtual = 1;
+
+function atualizaParedeAtual(){
+    if(comodo.length > 0) {
+        paredeAtual = comodo.length + 1;
+    }
+
+    $('#textoParede').text("Parede " + paredeAtual);
+}
+$(document).ready(() => {
+    atualizaParedeAtual();
+});
+
+$('#formButton').on('click',(e) => {
+    e.preventDefault();
+
+    let altura =$('#altura').val();
+    let largura =$('#largura').val();
+    let nmrPortas =$('#portas').val();
+    let nmrJanelas =$('#janelas').val();
+
+    let area = altura * largura;
+
+   if(area >= 1 && area <= 15) {
+       alert("muito bom meu garoto !!!");
+   }
+   else {
+       alert("Ruim");
+   }
+
+});
