a36fe106092d7aef5f97543e7a10f71c867a206d
Ana <acpedroso93@gmail.com>
2022-05-24 10:24:06 -0300

    Prosseguimos com a lógica das funções do formulário

--- scripts.js
+++ scripts.js
@@ -24,35 +24,96 @@
 // 'nmrJanelas':0
 // }
 
-let comodo =[];
+let comodo = [];
 let paredeAtual = 1;
+let mensagem = "";
+let campoMensagem = $("#mensagem");
 
-function atualizaParedeAtual(){
-    if(comodo.length > 0) {
+// funções
+function atualizaParedeAtual() {
+    if (comodo.length > 0) {
         paredeAtual = comodo.length + 1;
     }
 
     $('#textoParede').text("Parede " + paredeAtual);
 }
+
+function validaParede(altura, largura, nmrPortas, nmrJanelas) {
+    if (verificaAreaParede(altura, largura) == true) {
+
+    }
+    else{
+
+    }
+}
+
+function verificaAreaParede(altura, largura) {
+    let area = altura * largura;
+
+    if (area >= 1 && area <= 15)
+        return true;
+    else
+        return false;
+}
+function verificaAlturaParede(nmrPortas, altura) {
+    if ((nmrPortas > 0 && (altura - 0.3) >= 1.9) || nmrPortas == 0) 
+         return true;
+        else
+          return false;
+}
+
+function proporcaoParede(nmrPortas, nmrJanelas, altura, largura){
+    let area        =  (altura * largura) / 2;
+    let areaJanelas =  nmrJanelas * 2.4;
+    let areaPortas  =  nmrPortas * 1.52;
+
+    if ((areaJanelas + areaPortas) <= area) {
+        if (verificaAlturaParede(nmrPortas, altura) == true) {
+        comodo.push({
+            'altura':altura,
+            'largura':largura,
+            'nmrPortas':nmrPortas,
+            'nmrJanelas':nmrJanelas
+        });
+
+        console.log(comodo);
+        }
+        else {
+            mensagem = "A parede deve ter 30 cm a mais de altura que a porta!";
+            campoMensagem.removeClass("hidden");
+            campoMensagem.text(mensagem);
+        }
+    }
+    else {
+        mensagem = "O total de área das portas e janelas devem ser de no máximo 50% da área da parede!";
+        campoMensagem.removeClass("hidden");
+        campoMensagem.text(mensagem);
+    }
+}
+
+function validaParede(altura, largura, nmrPortas, nmrJanelas) {
+if (verificaAreaParede(altura, largura) == true) {
+    proporcaoParede(nmrPortas, nmrJanelas, altura, largura);
+}
+else {
+     mensagem = "A área da parede deve estar entre 1 e 15 metros quadrados!";
+     campoMensagem.removeClass("hidden");
+     campoMensagem.text(mensagem);
+}
+} 
+
 $(document).ready(() => {
     atualizaParedeAtual();
 });
 
-$('#formButton').on('click',(e) => {
+$('#formButton').on('click', (e) => {
     e.preventDefault();
 
-    let altura =$('#altura').val();
-    let largura =$('#largura').val();
-    let nmrPortas =$('#portas').val();
-    let nmrJanelas =$('#janelas').val();
+    let altura = $('#altura').val();
+    let largura = $('#largura').val();
+    let nmrPortas = $('#portas').val();
+    let nmrJanelas = $('#janelas').val();
 
-    let area = altura * largura;
-
-   if(area >= 1 && area <= 15) {
-       alert("muito bom meu garoto !!!");
-   }
-   else {
-       alert("Ruim");
-   }
-
+    validaParede(altura, largura, nmrPortas, nmrJanelas);
+    
 });
